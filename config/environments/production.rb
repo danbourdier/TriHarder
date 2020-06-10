8b15c169a36e980cb511dab5040ad80c48998353
DanBourdier <danbourdier@aol.com>
2020-06-10 10:55:55 -0400

    Updated heroku css image conflict with production.rb code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allows us to render non-compatible css with heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
