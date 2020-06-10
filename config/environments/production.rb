12b73505448f7f1ea6b4ebbf02fa8f0b916757ed
DanBourdier <danbourdier@aol.com>
2020-06-10 11:05:32 -0400

    heroku/css conflict fix take 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Allows us to render non-compatible css with heroku
   config.serve_static_assets = true
-  config.assets.compile = true
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
