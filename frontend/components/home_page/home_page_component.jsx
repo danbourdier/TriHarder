a55af81e025b0c7f6215a992421f908d66cb3047
DanBourdier <danbourdier@aol.com>
2020-06-12 02:01:16 -0400

    Corrected heroku bug

--- frontend/components/home_page/home_page_component.jsx
+++ frontend/components/home_page/home_page_component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import { login } from '../../actions/session_actions';
-import DashBoardContainer from '../dashboard/dashboard_container';
+
+
 import ActivityFeedContainer from '../activity_feed/activity_feed_container';
 import StatsContainer from '../stats/stats_container';
 import { Link } from 'react-router-dom';
@@ -29,9 +29,7 @@ const HomePageComponent = props => (
         </Link>
           
 
-        {/* <input type="radio" name="dash_tabs"/>
-        <input type="radio" name="dash_tabs"/>
-        <input type="radio" name="dash_tabs"/> */}
+       
       </nav>
 
     </header>
