62128a864c5ca5889aa6d1b0b65faddf61d12f9c
DanBourdier <danbourdier@aol.com>
2020-07-08 20:51:09 -0400

    created Gmaps container and presentational component

---
+++ frontend/components/mapping/route_map.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+
+class RouteMap extends Component {
+
+
+
+
+  render() {
+
+    return (
+      <div id="map-container" ref="map">
+        routeMap!
+      </div>
+    )
+
+  };
+
+
+}
+
+
+
+
+
+
+
+export default RouteMap;
---
+++ frontend/components/mapping/search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import RouteMap from "./route_map";
+
+const Search = props => (
+
+  <div>
+    {/* {RouteMap} */}
+    search route container ect. TESTTTT
+  </div>
+)
+
+
+
+export default Search;
