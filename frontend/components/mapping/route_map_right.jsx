6dc919b1249490be7eaabad197cd9fc08262a68f
DanBourdier <danbourdier@aol.com>
2020-07-11 20:54:09 -0400

    added boilerplate styling for map container

--- frontend/components/mapping/route_map_right.jsx
+++ frontend/components/mapping/route_map_right.jsx
@@ -22,10 +22,19 @@ class RouteMapRight extends Component {
 
     return (
       <section id="map-page-right" onClick={() => (this.handleClick())}>
-        <div>{Number(distance).toFixed(2)}</div>
-        {/* <div>{total_time} Minutes</div> */}
-        <button onClick={nullAllPoints}>clear all points</button>
-        <button onClick={nullPoint}>clear point</button>
+        <div className="map-page-right-distance-container">
+          <div style={{ fontSize: 11.25 + 'px' }}>
+            DISTANCE
+          </div>
+          <div style={{fontSize: 24 + "px", fontFamily: "Arial"}}>{Number(distance).toFixed(2)}<span> &nbsp;MI</span>
+          
+          </div>
+        </div>
+        {/* <div>ESTIMATED TIME: &nbsp;{total_time}&nbsp;Minutes</div> */}
+        <section className="map-page-right-buttons-container">
+          <button onClick={nullAllPoints}>Clear All Points</button>
+          <button onClick={nullPoint}>Clear Last Point</button>
+        </section>
           <strong>Points are not to exceed the "Z" point to not overload map requests</strong>
       </section>
     )
