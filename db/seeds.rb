13a725e4c91165577a92c78fcc0679905c293d0d
DanBourdier <danbourdier@aol.com>
2020-07-15 20:58:58 -0400

    heroku test update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Route.delete_all
 User.delete_all
+Route.delete_all
 
 
 
