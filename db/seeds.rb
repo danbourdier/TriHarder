43c9a8354fb2a2e2e90aaad54754a4c738eede37
DanBourdier <danbourdier@aol.com>
2020-07-15 21:33:27 -0400

    reseeded data to test db changes persisted

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,40 +32,40 @@ user5 = User.create!( first_name: "user5", last_name: "user5", email: "user5@use
 
 route1 = Route.create!(title: "Pompeii Speedrun", total_time: 2.10, activity: "run", 
   distance: 26.2, description: "Hydrate or Die", start_point: "Marathon, Greece", 
-  end_point: "Athens, Greece", user_id: 1
+  end_point: "Athens, Greece", user_id: demo.id
 )
-q
+
 route1a = Route.create!(title: "Bike Bonanza", total_time: 35.29, activity: "bike", 
   distance: 32.35, description: "Hills and scenery for the biker", start_point: "Fort Bragg, All American Expressway (Gruber Gate)", 
-  end_point: "Fort Bragg, 82nd Land", user_id: 1
+  end_point: "Fort Bragg, 82nd Land", user_id: demo.id
 )
 route1b = Route.create!(title: "Trail Trek Madness", total_time: 18.00, activity: "run", 
   distance: 8.6, description: "Bring your favorite trailrunners!!", start_point: "Fort Bragg, Gruber rd Motor Pool", 
-  end_point: "Fort Bragg, Zabitosky rd (Old 3rd Group trail entrance)", user_id: 1
+  end_point: "Fort Bragg, Zabitosky rd (Old 3rd Group trail entrance)", user_id: demo.id
 )
 route1c = Route.create!(title: "Operator's morning jog", total_time: 172.10, activity: "run", 
   distance: 38.60, description: "Enjoy a B-E-A-utiful run enjoyed by our finest, bring salt", start_point: "Fort Bragg, secret entrance(???)", 
-  end_point: "Space, ???", user_id: 1
+  end_point: "Space, ???", user_id: demo.id
 )
 
 route2 = Route.create!(title: "Styx River Dip", total_time: 5.01, 
   activity: "swim", distance: 999.0, description: "Don't drink the water", 
-  start_point: "Feneos", end_point: "Styx", user_id: 2
+  start_point: "Feneos", end_point: "Styx", user_id: demo.id
 )
 route2a = Route.create!(title: "Weekly Ruck", total_time: 174.23, 
   activity: "run", distance: 12.25, description: "Carbo-load the night before and Hydrate because you WILL sweat", 
-  start_point: "Fort Bragg, Gruber rd (range road intersection)", end_point: "Range Road exit", user_id: 2
+  start_point: "Fort Bragg, Gruber rd (range road intersection)", end_point: "Range Road exit", user_id: demo.id
 )
 
 route3 = Route.create!(title: "Inferno to Paradiso", total_time: 168.12,
   activity: "run", distance: 588.01, description: "From a little spark may burst a flame",
-  start_point: "First Circle of Hell", end_point: "9th Sphere", user_id: 3
+  start_point: "First Circle of Hell", end_point: "9th Sphere", user_id: demo.id
 )
 route3a = Route.create!(title: "Combat Fitness", total_time: 134.18,
   activity: "run", distance: 3.80, description: "Train like you will fight",
-  start_point: "Fort Bragg, Nasty Nick obstacle course", end_point: "Fort Bragg, Nasty Nick obstacle course end", user_id: 3
+  start_point: "Fort Bragg, Nasty Nick obstacle course", end_point: "Fort Bragg, Nasty Nick obstacle course end", user_id: demo.id
 )
 route3b = Route.create!(title: "Classified", total_time: 1159.32,
   activity: "run", distance: 120.00, description: "Classified",
-  start_point: "???", end_point: "???", user_id: 3
+  start_point: "???", end_point: "???", user_id: demo.id
 )
