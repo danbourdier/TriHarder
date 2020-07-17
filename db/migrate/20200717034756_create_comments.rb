fd066dc84f7693327cb3660d54e9205146063737
DanBourdier <danbourdier@aol.com>
2020-07-17 00:07:00 -0400

    pre-migration of comments table backend

---
+++ db/migrate/20200717034756_create_comments.rb
@@ -0,0 +1,15 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :author_username, null: false
+      t.string :body, null: false
+      t.datetime :date_created, null: false
+      t.integer :author_id, null: false
+
+
+      t.timestamps
+    end
+    add_index :comments, :author_id, unique: true
+    add_index :comments, :author_username, unique: true
+  end
+end
