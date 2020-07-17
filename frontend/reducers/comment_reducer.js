bf38bcd1a3bd07fb4a4f534753c5aae32ffcb244
DanBourdier <danbourdier@aol.com>
2020-07-17 13:43:34 -0400

    added boilerplate code for comment reducer

---
+++ frontend/reducers/comment_reducer.js
@@ -0,0 +1,16 @@
+// import two constants from actions
+import { RECEIVE_COMMENTS, RECEIVE_COMMENT } from '../actions/comment_actions'
+
+const commentReducer = (state = {}, action) => {
+  Object.freeze(state)
+
+  let nextState = Object.assign({}, state)
+  switch (action.type) {
+    case value:
+      
+      return state
+  
+    default:
+      return state
+  }
+}
