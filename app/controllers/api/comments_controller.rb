6d1591f93615256479071333092db09cd71f6486
DanBourdier <danbourdier@aol.com>
2020-07-17 01:41:50 -0400

    added boilerplate code for comment controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -6,23 +6,29 @@ class Api::CommentsController < ApplicationController
     render "api/comments/index"
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+
+    render "api/comments/show"
+  end
+
   def create
     @comment = Comment.new(comment_params)
 
     if @comment.save
-
+      render "api/comments/show"
     else
-      render_json @comment.errors.full_messages, status: 422
+      render json: @comment.errors.full_messages, status: 422
     end
   end
 
-  def edit
+  # def edit
 
-  end
+  # end
 
   private
   def comment_params
-    params.require(:comment).permit(:body, :author_email, :author_id, :)
+    params.require(:comment).permit(:body, :author_email, :author_id)
   end
 
 end
