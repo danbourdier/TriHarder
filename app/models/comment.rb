dacad343046925e23026ab34853b0f4b085a988d
DanBourdier <danbourdier@aol.com>
2020-07-17 00:32:42 -0400

    created comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+
+  validates 
+
+
+end
--- db/migrate/20200717034756_create_comments.rb
+++ db/migrate/20200717034756_create_comments.rb
@@ -5,10 +5,11 @@ class CreateComments < ActiveRecord::Migration[5.2]
       t.string :body, null: false
       t.datetime :date_created, null: false
       t.integer :author_id, null: false
-
+      t.integer :parent_comment_id
 
       t.timestamps
     end
+
     add_index :comments, :author_id, unique: true
     add_index :comments, :author_username, unique: true
   end
