115ad14c1406825c882bbc0a98a37e6e78ae831e
DanBourdier <danbourdier@aol.com>
2020-07-17 14:18:03 -0400

    keyed in comment errors reducer into errorsReducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import sessionErrorsReducer from './session_errors_reducer';
 import routesErrorsReducer from './routes_errors_reducer';
+import commentErrorsReducer from './comment_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   routes: routesErrorsReducer,
+  comments: commentErrorsReducer,
 });
 
 export default errorsReducer; 
