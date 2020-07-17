cc329df4234d00a3400b961124a5de11e0a679c5
DanBourdier <danbourdier@aol.com>
2020-07-17 01:37:28 -0400

    created api folder for comment jbuilder

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,13 +7,22 @@ class Api::CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
 
+    if @comment.save
+
+    else
+      render_json @comment.errors.full_messages, status: 422
+    end
   end
 
   def edit
 
   end
 
-
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :author_email, :author_id, :)
+  end
 
 end
