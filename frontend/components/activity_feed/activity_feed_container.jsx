74c732599b25b44070f147d5720a949a68b52463
DanBourdier <danbourdier@aol.com>
2020-07-18 15:04:46 -0400

    passed prop as state from containet

--- frontend/components/activity_feed/activity_feed_container.jsx
+++ frontend/components/activity_feed/activity_feed_container.jsx
@@ -4,12 +4,12 @@ import ActivityFeedIndex from './activity_feed_index';
 import { createComment } from '../../actions/comment_actions';
 
 const mSTP = state => ({
-  currentUser: state.entities.users[state.session.id]
-
+  currentUser: state.entities.users[state.session.id],
+  comments: state.entities.users[state.session.id].all_the_comments
 });
 
 const mDTP = dispatch => ({
   createComment: comment => dispatch(createComment(comment))
 });
- 
+
 export default connect(mSTP, mDTP)(ActivityFeedIndex);
