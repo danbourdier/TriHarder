92efa6208c5ff2a91512dccbe02f0bb187bcbfb1
DanBourdier <danbourdier@aol.com>
2020-07-20 14:22:46 -0400

    changed naming conventions for models

---
+++ app/controllers/api/connections_controller.rb
@@ -0,0 +1,36 @@
+class Api::ConnectionsController < ApplicationController
+
+  def index
+        @connections = connection.all
+        render :index
+    end
+
+   def create
+        @connection1 = Connection.new({requester: strong_params[:requester], requestee: strong_params[:requestee]}) 
+        @connection2 = Connection.new({requester: strong_params[:requestee], requestee: strong_params[:requester]}) 
+
+        if @connection1.save && @connection2.save
+            render :show
+        else
+            render json: ["Unable to accept connection"], status: 422
+        end
+    end
+
+    def destroy
+        @connection = connection.find(params[:id])
+        if @connection.destroy
+            render json: @connection
+        else
+            render json: ["Unable to delete connection"], status: 422
+        end 
+    end
+
+
+    private 
+
+    def strong_params
+        params.require(:connection).permit(:requester, :requestee)
+    end
+
+
+end
