9ace8ee59888cedbe4893aae526c8a2ce1d11766
DanBourdier <danbourdier@aol.com>
2020-07-20 17:52:44 -0400

    updated errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import routesErrorsReducer from './routes_errors_reducer';
 import commentErrorsReducer from './comment_errors_reducer';
+import connectionErrorsReducer from './connection_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   routes: routesErrorsReducer,
   comments: commentErrorsReducer,
+  connections: connectionErrorsReducer,
 });
 
 export default errorsReducer; 
