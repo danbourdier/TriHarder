c0b0cb3e382127b1bbdfabe23538c53e34cb1683
DanBourdier <danbourdier@aol.com>
2020-07-19 20:57:04 -0400

    added third dispatch to comment container

--- frontend/components/activity_feed/activity_feed_container.jsx
+++ frontend/components/activity_feed/activity_feed_container.jsx
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
 
 import ActivityFeedIndex from './activity_feed_index';
-import { createComment, getComments } from '../../actions/comment_actions';
+import { createComment, getComments, deleteComment } from '../../actions/comment_actions';
 
 const mSTP = state => ({
   currentUser: state.entities.users[state.session.id],
   comments: state.entities.comments,
-  // comments: state.entities.users[state.session.id].all_the_comments,
 });
 
 const mDTP = dispatch => ({
   createComment: comment => dispatch(createComment(comment)),
   getComments: () => dispatch(getComments()),
+  deleteComment: id => dispatch(deleteComment(id)),
 });
 
 export default connect(mSTP, mDTP)(ActivityFeedIndex);
