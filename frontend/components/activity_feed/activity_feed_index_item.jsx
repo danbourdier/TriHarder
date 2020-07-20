03eea59cc787b3a2775f65a09633f11e5e9cbad8
DanBourdier <danbourdier@aol.com>
2020-07-19 21:56:53 -0400

    successfully added comment deletion func

--- frontend/components/activity_feed/activity_feed_index_item.jsx
+++ frontend/components/activity_feed/activity_feed_index_item.jsx
@@ -2,7 +2,18 @@ import React, {Component } from 'react';
 
 
 class ActivityFeedIndexItem extends Component {
+  constructor(props) {
+    super(props)
+    this.commentId = this.props.comment.id;
+    this.deleteComment = this.props.deleteComment;
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+    return this.deleteComment(this.commentId)
+  }
 
   render() {
     let profilePicCollection = [camel, shark, turtle, bear, squirrel]
@@ -17,15 +28,15 @@ class ActivityFeedIndexItem extends Component {
       backgroundImage: 'url(' + rabbit + ')'
     }
 
-    let {body, the_author, the_author_email} = this.props.comment;
+    let { body, the_author, the_author_email } = this.props.comment;
 
-    console.log(this.props.comment)
+
     return (
       <section className="create-comment-container">
         <aside id="status-update-pic" style={ the_author === this.props.currentUserId ? userProfilePic : profilePic }></aside>
         <div className="ec-comments-and-posts">
           <div>
-            {the_author_email} <span className="delete-button">X</span>
+            {the_author_email} <span className="delete-button" onClick={ this.handleClick }>X</span>
           </div>
           {body}
         </div>
