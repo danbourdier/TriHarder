a6e7c796b3400304e6a0f85bdc7d073d9230aca0
DanBourdier <danbourdier@aol.com>
2020-07-22 13:28:07 -0400

    added connection requests util and actions

---
+++ frontend/actions/connection_requests_actions.js
@@ -0,0 +1,51 @@
+import * as ConnectionRequestAPIUtil from '../util/connection_request_api_util';
+
+export const RECEIVE_CONNECTION_REQUESTS = "RECEIVE_CONNECTION_REQUESTS";
+export const RECEIVE_CONNECTION_REQUEST = "RECEIVE_CONNECTION_REQUEST";
+export const REMOVE_CONNECTION_REQUEST = "REMOVE_CONNECTION_REQUEST";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+// action creators
+const receiveConnectionRequests = connectionRequests => ({
+  type: RECEIVE_CONNECTION_REQUESTS,
+  connectionRequests,
+})
+
+const receiveConnectionRequest = connectionRequest => ({
+  type: RECEIVE_CONNECTION_REQUEST,
+  connectionRequest,
+})
+
+const removeConnectionRequest = connectionRequestId => ({
+  type: REMOVE_CONNECTION_REQUEST,
+  connectionRequestId
+})
+
+const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+})
+
+// thunk action creators
+
+export const getConnectionRequests = () => dispatch => {
+  return (
+    ConnectionAPIUtil.getConnectionRequests()
+      .then(connectionRequests => dispatch(receiveConnectionRequests(connectionRequests)), errors => dispatch(receiveErrors(errors)))
+  )
+}
+
+export const createConnectionRequest = connectionRequest => dispatch => {
+  return (
+    ConnectionAPIUtil.createConnectionRequest(connectionRequest)
+      .then(connectionRequest => dispatch(receiveConnectionRequest(connectionRequest)), errors => dispatch(receiveErrors(errors)))
+  )
+}
+
+
+export const deleteConnectionRequest = connectionRequestId => dispatch => {
+  return (
+    ConnectionAPIUtil.deleteConnectionRequest(connectionRequestId)
+      .then(connectionRequest => dispatch(removeConnectionRequest(connectionRequestId)), errors => dispatch(receiveErrors(errors)))
+  )
+}
---
+++ frontend/util/connection_request_api_util.js
@@ -0,0 +1,21 @@
+
+export const createConnectionRequest = connectionRequest => (
+  $.ajax({
+    method: "Post",
+    url: "api/connections_requests",
+    data: { connectionRequest }
+  })
+)
+
+export const deleteConnectionRequest = connectionRequestId => (
+  $.ajax({
+    url: `api/connection_requests/${connectionRequestId}`,
+    method: "delete",
+  })
+)
+
+export const getConnectionRequests = () => (
+  $.ajax({
+    url: "api/connection_requests",
+  })
+)
