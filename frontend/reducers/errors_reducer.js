8c10176addf097b1e7c374e0107d6faf50a6bb9d
DanBourdier <danbourdier@aol.com>
2020-07-22 13:59:30 -0400

    added connection req error reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -4,12 +4,14 @@ import sessionErrorsReducer from './session_errors_reducer';
 import routesErrorsReducer from './routes_errors_reducer';
 import commentErrorsReducer from './comment_errors_reducer';
 import connectionErrorsReducer from './connection_errors_reducer';
+import connectionRequestErrorsReducer from './connection_request_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   routes: routesErrorsReducer,
   comments: commentErrorsReducer,
   connections: connectionErrorsReducer,
+  connectionRequests: connectionRequestErrorsReducer,
 });
 
 export default errorsReducer; 
