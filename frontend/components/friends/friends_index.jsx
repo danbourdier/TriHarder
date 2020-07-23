5b30e8aeb6fd76811647a143607c49ad094c1f46
DanBourdier <danbourdier@aol.com>
2020-07-23 18:55:09 -0400

    added css

--- frontend/components/friends/friends_index.jsx
+++ frontend/components/friends/friends_index.jsx
@@ -27,7 +27,7 @@ class FriendIndex extends Component {
     return (
       <div className="friend-index-component-container">
         <section className="friend-index-first-section">
-          <article><Link to="/friends">MY CONNECTIONS</Link></article>
+          <article id="friends-index-tab" ><Link to="/friends">MY CONNECTIONS</Link></article>
           <article><Link to="/find_friends">FIND CONNECTIONS</Link></article>
         </section>
 
