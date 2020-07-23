14edea4a7fa2faf7b15891bc93f5894d43da0d96
DanBourdier <danbourdier@aol.com>
2020-07-23 00:41:38 -0400

    added boilerplate code for friend index component

--- frontend/components/friends/friends_index.jsx
+++ frontend/components/friends/friends_index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
+import Friend from './friends_index_item';
+
 class FriendIndexItem extends Component {
+  constructor(props){
+    super(props)
+    this.connections = this.props.allConnections
+    this.createConnection = this.props.createConnection
+  }
 
   componentDidMount() {
-    // our get Connections() method
+    this.props.getConnections()
   }
 
   render() {
 
+    let index = Object.values(this.connections).map(connection => (
+      <Friend key={connection.id} connection={connection} />
+    ))
 
     
     return (
