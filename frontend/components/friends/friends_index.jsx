c1108525bc50521e01d1b687d1b2236efb83aee1
DanBourdier <danbourdier@aol.com>
2020-07-23 00:50:36 -0400

    added basecode to render connection index item

--- frontend/components/friends/friends_index.jsx
+++ frontend/components/friends/friends_index.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 import Friend from './friends_index_item';
 
-class FriendIndexItem extends Component {
+class FriendIndex extends Component {
   constructor(props){
     super(props)
-    this.connections = this.props.allConnections
-    this.createConnection = this.props.createConnection
+    this.connections = this.props.allConnections;
+    this.createConnection = this.props.createConnection;
+    this.deleteConnection = this.props.deleteConnection;
   }
 
   componentDidMount() {
@@ -16,18 +17,17 @@ class FriendIndexItem extends Component {
   render() {
 
     let index = Object.values(this.connections).map(connection => (
-      <Friend key={connection.id} connection={connection} />
+      <Friend key={connection.id} connection={connection} delete={this.deleteConnection} />
     ))
 
-    
     return (
-
-      <div>HELLLLOOOOOOO WOOORRRRLLLDDDD</div>
-
+      <div className="friend-index-component-container">
+        {index}
+      </div>
     )
   }
 
 
 }
 
-export default FriendIndexItem;
+export default FriendIndex;
---
+++ frontend/components/friends/friends_index_item.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class FriendIndexItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.connection = this.props.connection;
+    // this.connections = this.props.allConnections;
+    // this.createConnection = this.props.createConnection;
+    // this.deleteConnection = this.props.deleteConnection;
+  }
+
+  componentDidMount() {
+    // this.props.getConnections()
+  }
+
+  render() {
+
+    return (
+      <div className="friend-index-item-component-container">
+        {this.connection}
+      </div>
+    )
+  }
+
+
+}
+
+export default FriendIndexItem;
