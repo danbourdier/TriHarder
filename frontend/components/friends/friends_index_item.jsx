edded273d46ee44d2864bebaf726e45d6b35c1ab
DanBourdier <danbourdier@aol.com>
2020-07-23 18:47:33 -0400

    added css

--- frontend/components/friends/friends_index_item.jsx
+++ frontend/components/friends/friends_index_item.jsx
@@ -20,12 +20,19 @@ class FriendIndexItem extends Component {
     let { connection } = this.props;
     console.log(connection);
 
+    let profilePicCollection = [camel, shark, turtle, bear, squirrel]
+
+    let profilePic = {
+      backgroundSize: 'cover',
+      backgroundImage: 'url(' + profilePicCollection[Math.floor(Math.random() * profilePicCollection.length)] + ')'
+    };
+
     return (
       <article className="friend-index-item-component-container">
-        <div id="friend-index-item-pic"></div>
+        <div style={profilePic} id="friend-index-item-pic"></div>
         <aside className="friend-index-item-body">
-          <div id="ec-comment-first-section">{connection.connection.email}</div>
-          <div style={{ cursor: 'pointer', color: "#0076C0"}} onClick={ this.handleClick }>Delete Connection</div>
+          <div className="friend-index-item-body-email">{connection.connection.email}</div>
+          <div style={{ cursor: 'pointer', color: "#0076C0", fontSize: 11 }} onClick={ this.handleClick }>Delete Connection</div>
         </aside>
       </article>
     )
