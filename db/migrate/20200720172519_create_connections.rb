630a3a26c992117755c158d071c7734f0aade97a
DanBourdier <danbourdier@aol.com>
2020-07-24 00:28:56 -0400

    removed dead migration for heroku push

--- db/migrate/20200720172519_create_connections.rb
+++ db/migrate/20200720172519_create_connections.rb
@@ -1,6 +1,14 @@
 class CreateConnections < ActiveRecord::Migration[5.2]
   def change
+
     create_table :connections do |t|
+      t.integer :requester, null: false
+      t.integer :requestee, null: false
+
+      t.timestamps
     end
+
+    add_index :connections, [:requester, :requestee], unique: true
+    add_index :connections, [:requestee, :requester], unique: true
   end
 end
