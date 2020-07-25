62a600379b20a55968c6048b354a82b08a64e823
DanBourdier <danbourdier@aol.com>
2020-07-25 18:56:23 -0400

    added framework for replies

---
+++ frontend/components/activity_feed/replies.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+class Reply extends Component {
+
+
+
+  render () {
+    return  (
+      <div> A reply!</div>
+    )
+  }
+}
+
+export default Reply;
---
+++ frontend/components/activity_feed/replies_index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Reply from './replies';
+
+class RepliesIndex extends Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <Reply />
+      </div>
+    )
+  }
+
+}
+
+export default RepliesIndex;
