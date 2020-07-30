79069c59524b20acab4b5336d50e7ea80756ec79
DanBourdier <danbourdier@aol.com>
2020-07-29 21:33:06 -0400

    added css

--- frontend/components/friends/friend_finder_index_item.jsx
+++ frontend/components/friends/friend_finder_index_item.jsx
@@ -4,9 +4,17 @@ import React, { Component } from 'react';
 class SearchItem extends Component {
 
   render() {
+    console.log(this.props.result)
     return (
       <div className="search-item">
-        items!!!
+        <span>
+          <div style={{ backgroundImage: 'url(' + persona + ')', backgroundSize: 'cover'}} className="searched-item-pic"></div>
+          <div style={{height: 'fit-content', margin: 'auto' }}>{this.props.result.email}</div>
+        </span>
+
+        <button>
+          <div>ADD</div>
+        </button>
       </div>
     )
   }
