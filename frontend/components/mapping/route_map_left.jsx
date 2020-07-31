f65d8e919e59cd880a092018cbb1f42a252b04e0
DanBourdier <danbourdier@aol.com>
2020-07-30 23:07:31 -0400

    reimagined route creation interface

--- frontend/components/mapping/route_map_left.jsx
+++ frontend/components/mapping/route_map_left.jsx
@@ -66,47 +66,44 @@ class RouteMapLeft extends Component {
     return (
       <section id="map-page-left">
 
-          <aside>
-            <div>Choose Map Location</div>
+            <span id="ec-js11232">Choose Map Location</span>
+          <section id="map-page-left-first-section">
+          
             <input id="ec-address" placeholder="Search Location"/>
             <button id="ec-pan-submit"type="submit">Search</button>
-          </aside>
+          </section>
 
-          <aside>
-            <span>Route Name:</span>
-            <input  placeholder="Enter Title Here" 
-                    value={title}
-                    onChange={this.props.update('title')}/> 
-          </aside>
+          <section id="map-page-left-second-section">
+            <span>Route Details:</span>
+            <div id="route-map-left-details">
+              <input id="route-map-left-details-title" placeholder="Enter Title Here" 
+                      value={title}
+                      onChange={this.props.update('title')}/> 
 
-          <aside>
-            <span>Route Description:</span>
-            <input placeholder="Enter Description Here" 
-                  value={description} 
-                  onChange={this.props.update('description')} /> 
-          </aside>
+              <input id="route-map-left-details-title" placeholder="Enter Description Here"
+                value={description}
+                onChange={this.props.update('description')} /> 
 
-          <aside>
-            <span>Activity Type:</span>
-            <select id="ec-selectore-123" 
-                    name="activity" 
-                    placeholder="Choose Activity" 
-                    value={activity} 
-                    onChange={this.props.update('activity')}>
+              <select id="ec-selectore-123"
+                name="activity"
+                placeholder="Choose Activity"
+                value={activity}
+                onChange={this.props.update('activity')}>
 
-              {/* <option disabled >Choose an Activity</option> */}
-              <option value="">Choose an Activity</option>
-              <option value="Run">Run</option>
-              <option value="Walk">Walk</option>
-              <option value="Ruck">Ruck</option>
-              <option value="Bike">Bike</option>
-              <option value="Manic Sprint">Manic Sprint</option>
-            </select>
-          </aside>
-
-          <aside>
+                {/* <option disabled >Choose an Activity</option> */}
+                <option value="">Choose an Activity</option>
+                <option value="Run">Run</option>
+                <option value="Walk">Walk</option>
+                <option value="Ruck">Ruck</option>
+                <option value="Bike">Bike</option>
+                <option value="Manic Sprint">Manic Sprint</option>
+              </select>
+            </div>
+          </section>
+          
+          <section id="ec-button-1249-container">
             <button id="ec-button-1249-submit" type="submit" onClick={(this.handleClick(newRoute))} >Create Route!</button>
-          </aside>
+          </section>
 
       </section>
     )
