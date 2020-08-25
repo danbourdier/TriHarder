ec7f95f86279884ff5c26d6904d1df75ab1ceec0
DanBourdier <danbourdier@aol.com>
2020-08-25 12:53:52 -0400

    added successfull state props to route show through use of ownPropsgit add -Agit add -A

---
+++ frontend/components/routes/route.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Route extends Component {
+
+
+
+  render () {
+    debugger
+    return (
+      <div> hello </div>
+    )
+  }
+
+
+}
+
+export default Route;
--- frontend/components/routes/route_show_container.js
+++ frontend/components/routes/route_show_container.js
@@ -3,9 +3,16 @@ import { connect } from 'react-redux';
 // import { showRoutes } from '../../actions/route_actions';
 import Route from './route'
 
-const mSTP = state => ({
+const mSTP = (state, prevProps) => {
+  debugger
+  // the below takes advantage of mSTP second argument under the hoos and allows 
+    // access to our wildcard to use for our ingenious creations!
+  return {
+    route: state.entities.routes[Number(prevProps.match.params.routeId)],
+    currentUser: state.entities.users[state.session.id]
+  }
 
-})
+}
 
 const mDTP = dispatch => ({
 
