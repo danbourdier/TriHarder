b9d30cd1fa4b23f2915920dc09ce89ee541717cb
DanBourdier <danbourdier@aol.com>
2020-08-26 16:51:35 -0400

    added props for mapping container

--- frontend/components/mapping/search_container.jsx
+++ frontend/components/mapping/search_container.jsx
@@ -4,19 +4,25 @@ import Search from './search';
 import { createRoute } from '../../actions/route_actions'
 import { createComment } from '../../actions/comment_actions';
 
-const mSTP = state => ({
-  title: "",
-  total_time: 0,
-  activity: "",
-  distance: 0,
-  description: "",
-  start_point: "",
-  end_point: "",
-  currentErrors: state.errors.routes,
-  routes: state.entities.routes,
-  currentUser: state.entities.users[state.session.id],
-  lastRoute: Object.values(state.entities.routes)[Object.values(state.entities.routes).length - 1],
-});
+const mSTP = (state, ownProps) => {
+  debugger
+
+  return {
+    title: "",
+    total_time: 0,
+    activity: "",
+    distance: 0,
+    description: "",
+    start_point: "",
+    end_point: "",
+    currentErrors: state.errors.routes,
+    routes: state.entities.routes,
+    currentUser: state.entities.users[state.session.id],
+    lastRoute: Object.values(state.entities.routes)[Object.values(state.entities.routes).length - 1],
+    shownRoute: state.entities.routes[Number(ownProps.match.params.routeId)],
+  }
+
+};
 
 const mDTP = dispatch => ({
   createRoute: route => dispatch(createRoute(route)),
