f10a002758b4e607726fc4bba01ad613a2835bd5
DanBourdier <danbourdier@aol.com>
2020-08-25 21:13:04 -0400

    added presentational component basecode for show routes

--- frontend/components/routes/route.jsx
+++ frontend/components/routes/route.jsx
@@ -3,15 +3,37 @@ import React, {Component} from 'react';
 class Route extends Component {
   constructor(props) {
     super(props)
-    
+
+    this.route = this.props.route
+
+  }
+
+  componentDidMount() {
+    this.props.getRoutes();
   }
 
 
   render () {
-    debugger
-    return (
-      <div> hello </div>
-    )
+
+    if (this.route) {
+      const { total_time, distance, activity, description, 
+        title, created_at, updated_at 
+      } = this.route
+
+      return (
+        <main>
+
+          <section>{title}</section>
+
+        </main>
+      )
+
+    } else {
+      return <div></div>
+    }
+    
+
+    
   }
 
 
--- frontend/components/routes/route_show_container.js
+++ frontend/components/routes/route_show_container.js
@@ -2,20 +2,23 @@ import { connect } from 'react-redux';
 
 import { showRoute, deleteRoute, showRoutes } from '../../actions/route_actions';
 import Route from './route'
+import { editRoute } from '../../util/route_api_util';
 
 const mSTP = (state, ownProps) => {
-  debugger
   // the below takes advantage of mSTP second argument under the hoos and allows 
     // access to our wildcard to use for our ingenious creations!
   return {
     route: state.entities.routes[Number(ownProps.match.params.routeId)],
-    currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id],
   }
 
 }
 
 const mDTP = dispatch => ({
-
+  getRoutes: () => dispatch(showRoutes()),
+  getRoute: route => dispatch(showRoute(route)),
+  deleteRoute: routeId => dispatch(deleteRoute(routeId)),
+  editRoute: route => dispatch(editRoute(route)),
 
 });
 
