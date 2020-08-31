1aea4a3223e8351a321bb0f79bdd03c960a88e27
DanBourdier <danbourdier@aol.com>
2020-08-31 07:09:57 -0400

    fixed pesky header popups

--- frontend/components/header/header_main.jsx
+++ frontend/components/header/header_main.jsx
@@ -8,48 +8,51 @@ class HeaderMain extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { community: true, routes: true }
+    this.state = { community: true, routes: true };
+
+    this.flipCommunity = this.flipCommunity.bind(this);
+    this.flipRoutes = this.flipRoutes.bind(this);
   }
 
   componentDidMount() {
-    let communityTargetOne = document.getElementById("uscn9138");
-    let communityTargetTwo = document.getElementById("uscn9138123");
+    // let communityTargetOne = document.getElementById("uscn9138");
+    // let communityTargetTwo = document.getElementById("uscn9138123");
 
-    communityTargetOne.addEventListener("mouseenter", () => {
-      if (!this.state.routes) {
-        this.flipRoutes()
-      }
-      this.flipCommunity();
-      }
-    );
+    // communityTargetOne.addEventListener("mouseenter", () => {
+    //   if (!this.state.routes) {
+    //     this.flipRoutes()
+    //   }
+    //   this.flipCommunity();
+    //   }
+    // );
 
-    communityTargetTwo.addEventListener("mouseout", () => {
-      this.flipCommunity();
-      }
-    );
+    // communityTargetTwo.addEventListener("mouseout", () => {
+    //   this.flipCommunity();
+    //   }
+    // );
 
-    let routeTargetOne = document.getElementById("ji-8291")
-    let routeTargetTwo = document.getElementById("ji-8291765")
+    // let routeTargetOne = document.getElementById("ji-8291")
+    // let routeTargetTwo = document.getElementById("ji-8291765")
 
-    routeTargetOne.addEventListener("mouseenter", () => {
-      if (!this.state.community) {
-        this.flipCommunity()
-      }
-      this.flipRoutes();
-    }
-    );
-    routeTargetTwo.addEventListener("mouseout", () => {
-      this.flipRoutes();
-    }
-    );
+    // routeTargetOne.addEventListener("mouseenter", () => {
+    //   if (!this.state.community) {
+    //     this.flipCommunity()
+    //   }
+    //   this.flipRoutes();
+    // }
+    // );
+    // routeTargetTwo.addEventListener("mouseout", () => {
+    //   this.flipRoutes();
+    // }
+    // );
 
   }
 
   flipCommunity() {
-    this.setState({ community: (!this.state.community) })
+    return this.setState({ community: (!this.state.community) })
   }
   flipRoutes() {
-    this.setState({ routes: (!this.state.routes) })
+    return this.setState({ routes: (!this.state.routes) })
   }
 
   render() {
@@ -70,22 +73,28 @@ class HeaderMain extends Component {
           </div>
 
           <div className="link-container">
-            <section id="uscn9138" className="ec-123-5-link-community" >Community</section>
-            <div id="uscn9138123">
-              <div  className={this.state.community ? "display-none" : "community-header-pic-dropdown-container-visible"} >
+            <section id="uscn9138" 
+                     className="ec-123-5-link-community" 
+                     onMouseEnter={this.flipCommunity} 
+                     onMouseLeave={this.flipCommunity}><span>Community</span>
+              <div  className={this.state.community ? "display-none" : "community-header-pic-dropdown-container-visible"}>
                 <Link className="header-button" to="/friends">Friends</Link>
                 <Link className="header-button" to="/activity_feed">Activity Feed</Link>
                 <a className="header-button" href="https://www.linkedin.com/in/danielbourdier/">Import Talent</a>
               </div>
-            </div>
+            </section>
 
-            <section id="ji-8291" className="ec-123-5-link-routes">Routes</section>
-            <div id="ji-8291765" >
+
+            <section id="ji-8291" 
+                     className="ec-123-5-link-routes"
+                     onMouseEnter={this.flipRoutes}
+                     onMouseLeave={this.flipRoutes}><span>Routes</span>
               <div className={this.state.routes ? "display-none" : "routes-header-pic-dropdown-container-visible"}>
                 <Link className="header-button" to="/create_workout">Log Workout</Link>
                 <Link className="header-button" to="/map">Map Route</Link>
               </div>
-            </div>
+            </section>
+              
 
             <a className="ec-123-5-link" href="https://github.com/danbourdier">GitHub</a>
             <a className="ec-123-5-link" href="https://angel.co/u/daniel-bourdier">AngelList</a>
