9986a6cd1d2c2c7dd7783aa799fcda8a1317cdde
DanBourdier <danbourdier@aol.com>
2020-09-15 20:15:57 -0400

    removed dead code

--- frontend/components/mapping/route_map_main.jsx
+++ frontend/components/mapping/route_map_main.jsx
@@ -159,10 +159,6 @@ class RouteMap extends Component {
     geocoder.geocode({ address: address }, (results, status) => {
       if (status === "OK") {
         resultsMap.setCenter(results[0].geometry.location);
-        // new google.maps.Marker({
-        //   map: resultsMap,
-        //   position: results[0].geometry.location
-        // });
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
